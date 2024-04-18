960d7112c8bb2690f0462036100dc55a5c3117c9
Matthew Li <m739li@uwaterloo.ca>
2024-04-18 02:40:43 -0400

    Added flagging capability

--- script.js
+++ script.js
@@ -16,24 +16,49 @@ document.addEventListener("mouseup", function() {
 // Setup
 let cellmouseout = function(event) {
     event.currentTarget.className = "cell closed";
-}
+};
 
 let cellmousedown = function(event) {
-    event.currentTarget.className = "cell pressed";
-}
+    if (event.which === 1 && event.currentTarget.className != "cell flagged") { // left mouse button
+        event.currentTarget.className = "cell pressed";
+    } else if (event.which === 3) { // right mouse button
+        if (event.currentTarget.className != "cell type1") { // TODO: regex for all numbers
+            if (event.currentTarget.className == "cell flagged") {
+                event.currentTarget.className = "cell closed";
+                event.currentTarget.addEventListener("mouseout", cellmouseout);
+            } else {
+                event.currentTarget.className = "cell flagged";
+                event.currentTarget.removeEventListener("mouseout", cellmouseout);
+            }
+        }
+    }
+};
 
 let cellmouseup = function(event) {
-    event.currentTarget.className = "cell type1"; // TODO: determine the number that should be displayed
-    event.currentTarget.removeEventListener("mouseout", cellmouseout);
-    event.currentTarget.removeEventListener("mousedown", cellmousedown);
-}
+    if (event.which === 1 && event.currentTarget.className != "cell flagged") {
+        event.currentTarget.className = "cell type1"; // TODO: determine the number that should be displayed
+        event.currentTarget.removeEventListener("mouseout", cellmouseout);
+        event.currentTarget.removeEventListener("mousedown", cellmousedown);
+    }
+};
 
 let cellmouseenter = function(event) {
-    if (event.currentTarget.className != "cell type1") { // TODO: regex for all numbers?
+    if (event.currentTarget.className != "cell type1") { // TODO: regex for all numbers
         event.currentTarget.addEventListener("mousedown", cellmousedown);
         if (pressed) { 
             event.currentTarget.className = "cell pressed";
         };
+    }
+};
+
+let placeflag = function(event) {
+    event.preventDefault();
+    if (event.currentTarget.className != "cell type1") { // TODO: regex for all numbers
+        if (event.currentTarget.className == "cell flagged") {
+            event.currentTarget.className = "cell closed";
+        } else {
+            event.currentTarget.className = "cell flagged";
+        }
     }
 }
 
@@ -41,4 +66,7 @@ document.querySelectorAll(".cell").forEach(e => {
     e.addEventListener("mouseenter", cellmouseenter);
     e.addEventListener("mouseout", cellmouseout);
     e.addEventListener("mouseup", cellmouseup);
+    e.addEventListener("contextmenu", function(event) {
+        event.preventDefault();
+    });
 });
