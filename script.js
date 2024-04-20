88e372b4d8aac7d17e96b335fe1fdf4517439f62
Matthew Li <m739li@uwaterloo.ca>
2024-04-20 19:36:09 -0400

    Fixed flagging/pressing/exploding combinations

--- script.js
+++ script.js
@@ -22,32 +22,34 @@ document.addEventListener("mouseup", function() {
 let ws;
 
 let cellmouseout = function(event) {
-    event.currentTarget.className = "cell closed";
+    if (event.currentTarget.className === "cell pressed") {
+        event.currentTarget.className = "cell closed";
+    }
 };
 
 let cellmousedown = function(event) {
-    if (event.button === 0 && event.currentTarget.className != "cell flagged") { // left mouse button
-        event.currentTarget.className = "cell pressed";
+    if (event.button === 0) {
+        if (event.currentTarget.className === "cell closed") { // left mouse button
+            event.currentTarget.className = "cell pressed";
+        } else if (event.currentTarget.className === "cell type1") { // TODO: regex for all numbers
+            console.log("chord");
+            // TODO: chord
+        }
     } else if (event.button === 2) { // right mouse button
-        if (event.currentTarget.className != "cell type1") { // TODO: regex for all numbers
-            if (event.currentTarget.className == "cell flagged") {
+        if (event.currentTarget.className !== "cell type1" && event.currentTarget.className !== "cell exploded") { // TODO: regex for all numbers
+            if (event.currentTarget.className === "cell flagged") {
                 event.currentTarget.className = "cell closed";
-                event.currentTarget.addEventListener("mouseout", cellmouseout);
             } else {
                 event.currentTarget.className = "cell flagged";
-                event.currentTarget.removeEventListener("mouseout", cellmouseout);
             }
         }
     }
 };
 
 let cellmouseup = function(event) {
-    if (event.which === 1 && event.currentTarget.className != "cell flagged") {
+    if (event.which === 1 && event.currentTarget.className !== "cell flagged" && event.currentTarget.className !== "cell exploded" && event.currentTarget.className !== "cell type1") { // TODO: regex for all numbers
         console.log("revealing cell");
         event.currentTarget.className = "cell type1"; // TODO: determine the number that should be displayed
-        event.currentTarget.removeEventListener("mouseout", cellmouseout);
-        event.currentTarget.removeEventListener("mousedown", cellmousedown);
-        event.currentTarget.removeEventListener("mouseup", cellmouseup);
         revealCell(event);
     }
 };
@@ -58,7 +60,7 @@ let revealCell = function(event) {
 }
 
 let cellmouseenter = function(event) {
-    if (event.currentTarget.className != "cell type1") { // TODO: regex for all numbers
+    if (event.currentTarget.className !== "cell type1" && event.currentTarget.className !== "cell exploded" && event.currentTarget.className !== "cell flagged") { // TODO: regex for all numbers
         event.currentTarget.addEventListener("mousedown", cellmousedown);
         if (leftPressed) { 
             event.currentTarget.className = "cell pressed";
@@ -67,7 +69,6 @@ let cellmouseenter = function(event) {
 };
 
 document.querySelectorAll(".cell").forEach(e => {
-    console.log("im here");
     e.addEventListener("mouseenter", cellmouseenter);
     e.addEventListener("mouseout", cellmouseout);
     e.addEventListener("mouseup", cellmouseup);
@@ -83,7 +84,7 @@ connect().then(function(websocket) {
     
     ws.addEventListener("message", (message) => {
         message = JSON.parse(message.data);
-        console.log("message: " + message);
+        console.log("message: ", message);
         if (message.type === "niceTry") {
             console.log("lol");
         } else if (message.type === "revealCell") {
