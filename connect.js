a6855cdb92521b85fc6dd94c0712b556645774d8
Jeffrey Zhen <60681496+jlrzhen@users.noreply.github.com>
2024-05-08 01:17:24 +0000

    enable loader and connection error elements

--- connect.js
+++ connect.js
@@ -1,23 +1,27 @@
 export function connect() {
     return new Promise(function(resolve, reject) {
-        var ws = new WebSocket('wss://minesweepercoop-6gn2.onrender.com');
-        // document.getElementById('connectionfailed').style.display = "none";
-        // document.getElementById('loader').style.display = "inline";
-        // document.getElementById('waitingforserver').style.display = "block";
+        var ws = new WebSocket('wss://shiny-space-garbanzo-vrjrvj4466x2wrj4-10000.app.github.dev/');
+        document.getElementById('connectionfailed').style.display = "none";
+        document.getElementById('loader').style.display = "inline";
+        document.getElementById('waitingforserver').style.display = "block";
         // document.body.style.backgroundColor = "#645a5a";
         ws.onopen = function() {
             console.log("connection opened");
-            // document.getElementById('loader').style.display = "none";
-            // document.getElementById('waitingforserver').style.display = "none";
+            document.getElementById('playersetup').style.display = "inline";
+            document.getElementById('loader').style.display = "none";
+            document.getElementById('waitingforserver').style.display = "none";
             resolve(ws);
         };
         ws.onerror = function(err) {
             // document.body.style.backgroundColor = "#CCCCCC";
-            // document.getElementById('loader').style.display = "none";
-            // document.getElementById('waitingforserver').style.display = "none";
-            // document.getElementById('connectionfailed').style.display = "inline";
+            document.getElementById('loader').style.display = "none";
+            document.getElementById('waitingforserver').style.display = "none";
+            document.getElementById('connectionfailed').style.display = "inline";
             console.log("some websocket error: ", err);
             reject(err);
         };
     });
+}
+document.getElementById('tryconnectagain').onclick = function() {
+    location.reload();
 }
