e31686c026466b635baee62aba7c9043409711bd
Matthew Li <m739li@uwaterloo.ca>
2024-05-10 00:13:59 -0400

    Fix connection prompt on fake connections

--- connect.js
+++ connect.js
@@ -1,10 +1,12 @@
 export function connect(firstTime) {
     return new Promise(function(resolve, reject) {
         var ws = new WebSocket('wss://minesweepercoop-6gn2.onrender.com');
-        document.getElementById('connectionfailed').style.display = "none";
-        document.getElementById('loader').style.display = "block";
-        document.getElementById('waitingforserver').style.display = "block";
-        // document.body.style.backgroundColor = "#645a5a";
+        if (firstTime) {
+            document.getElementById('connectionfailed').style.display = "none";
+            document.getElementById('loader').style.display = "block";
+            document.getElementById('waitingforserver').style.display = "block";
+            // document.body.style.backgroundColor = "#645a5a";
+        }
         ws.onopen = function() {
             console.log("connection opened");
             if (firstTime) {
