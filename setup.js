9075ba256c3ae6ca37ad0050faea829224097290
Matthew Li <m739li@uwaterloo.ca>
2024-05-09 23:08:19 -0400

    Oops

--- setup.js
+++ setup.js
@@ -1,7 +1,7 @@
 import { cellmouseout, cellmouseup, cellmouseenter } from './mouseEvents.js';
 import { generateBoard } from './generateBoard.js';
 import { wsMsgHandler } from './wsMsgHandler.js';
-import { connect } from './devconnect.js';
+import { connect } from './connect.js';
 
 export function setupCells() { // * Does this function belong here? Little bit circular
     document.querySelectorAll(".cell").forEach(e => {
