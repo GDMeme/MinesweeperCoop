ceb37c4b6844cefd85e23b46bf9e3c2651f06a4b
Jeffrey Zhen <jlrzhen@uwaterloo.ca>
2025-01-13 00:36:50 -0500

    add dropdown selector for dev game server

--- developerMode.js
+++ developerMode.js
@@ -7,16 +7,54 @@
  * Returns server url based on detected environment
  */
 export function getGameServerUrl() {    
-    const onRenderServerUrl = 'wss://minesweepercoop-6gn2.onrender.com'
-    const localServerPort = '8080'
+    const onRenderServerUrl = 'wss://minesweepercoop-6gn2.onrender.com';
     const hostname = window.location.hostname;
     
     console.log(`host: ${hostname}`);
 
     if(hostname == "localhost" || hostname == "127.0.0.1") {
         console.log("DEV MODE ENABLED");
-        return `wss://${hostname}:${localServerPort}`
+        document.getElementById('developermode').style.display = "block";
+        let gameServerUrl = document.querySelector('#developermodebannerserverselector').value;
+        if(cookieExists("gameServerUrl")) {
+            gameServerUrl = getCookie("gameServerUrl");
+            document.querySelector('#developermodebannerserverselector').value = gameServerUrl; 
+        }
+        return gameServerUrl;
     }
         
-    return onRenderServerUrl
+    document.getElementById('developermode').style.display = "none";
+    return onRenderServerUrl;
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function cookieExists(cname) {
+    return getCookie(cname) != "";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+document.getElementById('developermodebannerserverselector').onchange = function() {
+    setCookie("gameServerUrl", document.querySelector('#developermodebannerserverselector').value, 1);
+    console.log(getCookie("gameServerUrl"));
+    location.reload();
 }
